cb87b0bbe1e4c726d9650a52e443a0fe4aeea6da
yan <1933679510@qq.com>
2020-11-07 10:46:51 +0800

    再次上传

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
+use Log;
+
 class TextController extends Controller
 {
 
